8f1cf75c1b3022e4d086fc4409c37a9a9b78bff1
Ririena <nerllysurina@gmail.com>
2024-03-10 14:48:44 +0700

    Debug

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Card, Button, Image } from "@nextui-org/react";
 import { motion } from "framer-motion";
-
 import RetrieveUserFirst from "../Main/RetrieveUserFirst";
 import { supabase } from "../../utils/supabase";
-import {Grid, GridItem} from "@chakra-ui/react"
+import { Grid, GridItem } from "@chakra-ui/react";
+
 async function retrieveUser() {
   try {
     const {
@@ -25,88 +25,102 @@ async function retrieveUser() {
 
 retrieveUser();
 
-
+const HeroSection = () => {
+  const words = [
+    {
+      text: "Auto",
+      className: "text-md",
+    },
+    {
+      text: "Memories",
+      className: "text-md",
+    },
+    {
+      text: "Started",
+      className: "text-md",
+    },
+  ];
 
   return (
     <div className="bg-gray-100 py-20">
-    <div className="container mx-auto flex justify-between items-center">
-      <motion.div
-        className="max-w-lg mx-4"
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.1 }}
-      >
-        <h1 className="text-3xl md:text-4xl lg:text-4xl font-bold mb-6">
-          Letter For Tomorrow
-        </h1>
-        <p className="text-gray-600 text-base md:text-lg lg:text-xl">
-          it is a webapps whose concept is almost the same as NGL(NotGonnaLie
-          Apps) but with additional features, where you represent as Violet
-          Evergarden or a mailer which sends letters to people there, and
-          that's why it is called Letter4Tomorrow
-        </p>
-        <Button color="primary" variant="flat" className="mt-4">
-          Get Started!!
-        </Button>
-      </motion.div>
-  
-      <div className="flex justify-end gap-3">
+      <div className="container mx-auto flex justify-between items-center">
         <motion.div
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.9 }}
-          initial={{ opacity: 0, x: -50 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5, delay: 0.2 }}
+          className="max-w-lg mx-4"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.1 }}
         >
-          <Card
-            className="shadow-lg max-w-md"
-            whileTap={{ scale: 0.95 }} // Tambahkan efek saat kartu ditekan
-          >
-            <motion.div
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5 }}
-            >
-              <Image
-                isBlurred={true}
-                src="/Mails.gif"
-                alt="Landscape"
-                className="hidden lg:block xl:block w-full h-96 object-cover rounded-t-lg"
-              />
-            </motion.div>
-          </Card>
+          <h1 className="text-3xl md:text-4xl lg:text-4xl font-bold mb-6">
+            Letter For Tomorrow
+          </h1>
+          <p className="text-gray-600 text-base md:text-lg lg:text-xl">
+            it is a webapps whose concept is almost the same as NGL(NotGonnaLie
+            Apps) but with additional features, where you represent as Violet
+            Evergarden or a mailer which sends letters to people there, and
+            that's why it is called Letter4Tomorrow
+          </p>
+          <Button color="primary" variant="flat" className="mt-4">
+            Get Started!!
+          </Button>
         </motion.div>
-  
-        <motion.div
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.9 }}
-          initial={{ opacity: 0, x: 50 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5, delay: 0.2 }}
-        >
-          <Card
-            className="overflow-hidden shadow-lg mr-5"
-            style={{ width: "max-content" }}
-            whileTap={{ scale: 0.95 }} // Tambahkan efek saat kartu ditekan
+
+        <div className="flex justify-end gap-3">
+          <motion.div
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.9 }}
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
           >
-            <motion.div
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5 }}
+            <Card
+              className="shadow-lg max-w-md"
+              whileTap={{ scale: 0.95 }} // Tambahkan efek saat kartu ditekan
             >
-              <Image
-                isBlurred={true}
-                src="/violet.jpg"
-                alt="Portrait"
-                className="w-full h-96 object-cover rounded-t-lg bg-gradient-to-r bg-transparent"
-              />
-            </motion.div>
-          </Card>
-        </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+              >
+                <Image
+                  isBlurred={true}
+                  src="/Mails.gif"
+                  alt="Landscape"
+                  className="hidden lg:block xl:block w-full h-96 object-cover rounded-t-lg"
+                />
+              </motion.div>
+            </Card>
+          </motion.div>
+
+          <motion.div
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.9 }}
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+          >
+            <Card
+              className="overflow-hidden shadow-lg mr-5"
+              style={{ width: "max-content" }}
+              whileTap={{ scale: 0.95 }} // Tambahkan efek saat kartu ditekan
+            >
+              <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+              >
+                <Image
+                  isBlurred={true}
+                  src="/violet.jpg"
+                  alt="Portrait"
+                  className="w-full h-96 object-cover rounded-t-lg bg-gradient-to-r bg-transparent"
+                />
+              </motion.div>
+            </Card>
+          </motion.div>
+        </div>
       </div>
+      <RetrieveUserFirst />
     </div>
-    <RetrieveUserFirst />
-  </div>
   );
 };
 
